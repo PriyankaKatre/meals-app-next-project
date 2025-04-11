de6fd76ce21e716ef3b4540ce76f0a0831bb687a
Priyanka Katre <priyankack1707@gmail.com>
2025-04-11 16:12:42 +0000

    error page

---
+++ app/meals/error.js
@@ -0,0 +1,13 @@
+'use client'
+
+const Error = () =>{
+    return (
+        <main className="error">
+            <h1>An Error occured!</h1>
+            <p>Failed to fetch meal data, please try agian later.</p>
+        </main>
+    )
+}
+
+export default Error 
+
